11c7096c6bc959dd9da4eaa762a0b4a86531a4c5
celestialalpaca <denizgumus1996@hotmail.com>
2024-07-29 12:16:19 +0300

    1

---
+++ src/main/java/com/candenizgumus/humanresourcesapp/HumanResourcesAppApplication.java
@@ -0,0 +1,15 @@
+package com.candenizgumus.humanresourcesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesAppApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HumanResourcesAppApplication.class, args);
+    }
+
+}

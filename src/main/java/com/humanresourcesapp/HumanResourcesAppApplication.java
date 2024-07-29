c29a19dac549fdbc5d20b9498b4433d57cf905c0
celestialalpaca <denizgumus1996@hotmail.com>
2024-07-29 12:19:52 +0300

    2

--- src/main/java/com/humanresourcesapp/HumanResourcesAppApplication.java
+++ src/main/java/com/humanresourcesapp/HumanResourcesAppApplication.java
@@ -1,4 +1,4 @@
-package com.candenizgumus.humanresourcesapp;
+package com.humanresourcesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/humanresourcesapp/HumanResourcesAppApplicationTests.java
+++ src/test/java/com/humanresourcesapp/HumanResourcesAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.candenizgumus.humanresourcesapp;
+package com.humanresourcesapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

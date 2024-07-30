34fecc600c5849f6ece95f0ba9711485200f2d7c
emiresen <emiresen246@gmail.com>
2024-07-30 12:57:20 +0300

    Entity fields added

--- src/main/java/com/humanresourcesapp/entities/BaseEntity.java
+++ src/main/java/com/humanresourcesapp/entities/BaseEntity.java
@@ -3,21 +3,20 @@ package com.humanresourcesapp.entities;
 import com.humanresourcesapp.entities.enums.EStatus;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
-@SuperBuilder
+@Builder
 @Data
-@MappedSuperclass
-public abstract class BaseEntity {
+public abstract class BaseEntity
+{
     @CreationTimestamp
     private Long createdAt;
     @UpdateTimestamp

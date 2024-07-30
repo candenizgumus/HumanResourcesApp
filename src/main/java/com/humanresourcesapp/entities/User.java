e6e1c2eb412596487ee5b6cd05985a9fca4f73f9
celestialalpaca <denizgumus1996@hotmail.com>
2024-07-30 11:10:38 +0300

    user

--- src/main/java/com/humanresourcesapp/entities/User.java
+++ src/main/java/com/humanresourcesapp/entities/User.java
@@ -1,5 +1,20 @@
 package com.humanresourcesapp.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "tbluser")
 public class User
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }

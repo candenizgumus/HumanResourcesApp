c45158ee4c7ee96ec90cc40bfe65c8f5e38c94f3
hcaslan <hcaslan.ozen@gmail.com>
2024-07-30 13:19:10 +0300

    base entity changed

--- src/main/java/com/humanresourcesapp/entities/BaseEntity.java
+++ src/main/java/com/humanresourcesapp/entities/BaseEntity.java
@@ -20,9 +20,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseEntity
 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
     @CreationTimestamp
     LocalDateTime createdAt;
     @UpdateTimestamp

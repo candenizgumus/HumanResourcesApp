96bc5caa3075d462f0b584d55232fbc00ba1deb2
celestialalpaca <denizgumus1996@hotmail.com>
2024-07-30 16:11:58 +0300

    Authcontroller, user added

--- src/main/java/com/humanresourcesapp/services/AuthService.java
+++ src/main/java/com/humanresourcesapp/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.humanresourcesapp.services;
 
 import com.humanresourcesapp.entities.Auth;
+import com.humanresourcesapp.entities.User;
 import com.humanresourcesapp.repositories.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,23 @@ import java.util.Optional;
 public class AuthService
 {
     private final AuthRepository authRepository;
+    private final UserService userService;
 
     public Optional<Auth> findById(Long id)
     {
         return authRepository.findById(id);
+    }
+
+    public Boolean login(Auth auth)
+    {
+        Auth authUser = authRepository.save(auth);
+        userService.save(User
+                .builder()
+                .authId(authUser.getId())
+                .email(authUser.getEmail())
+                .userType(authUser.getUserType())
+                .build());
+
+        return false;
     }
 }

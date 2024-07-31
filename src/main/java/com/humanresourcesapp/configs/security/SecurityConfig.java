3ab7c073f14aaa0e0b6f4115b073de86c19debcc
hcaslan <hcaslan.ozen@gmail.com>
2024-07-31 09:35:53 +0300

    PasswordEncoder

--- src/main/java/com/humanresourcesapp/configs/security/SecurityConfig.java
+++ src/main/java/com/humanresourcesapp/configs/security/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.humanresourcesapp.configs.security;
 
+import com.humanresourcesapp.services.AuthService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +22,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig
 {
     private final JwtTokenFilter jwtTokenFilter;
+    private final AuthService authService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
@@ -31,10 +41,17 @@ public class SecurityConfig
                         .requestMatchers("/company/**").permitAll()
                         .requestMatchers("auth/login/**","auth/register/**","auth/registerwithrabbit/**").permitAll()
                         .requestMatchers("auth/activationwithrabbit/**","auth/passwordresetmail/**","auth/changepasswordwithresetcode/**").permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+                .authenticationProvider(authProvider());
                 //.formLogin(Customizer.withDefaults());
 
 
         return httpSecurity.build();
+    }
+    @Bean
+    public AuthenticationProvider authProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return authenticationProvider;
     }
 }

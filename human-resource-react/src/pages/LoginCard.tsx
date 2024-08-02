1d0e9419e091d2041f95b2e6d98f05e6bfa7ed3f
candenizgumus <denizgumus1996@hotmail.com>
2024-08-02 08:19:00 +0300

    LoginCard error handler

--- human-resource-react/src/pages/LoginCard.tsx
+++ human-resource-react/src/pages/LoginCard.tsx
@@ -8,7 +8,6 @@ import {Link, useNavigate} from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import {SportsTennis} from "@mui/icons-material";
 import {useDispatch} from "react-redux";
@@ -16,47 +15,35 @@ import {fetchFindUserByToken, fetchLogin} from "../store/feature/authSlice";
 import {HumanResources} from "../store";
 
 import {useState} from "react";
-
+import {Alert, AlertTitle} from "@mui/material";
 
 export default function LoginCard() {
-
-
-
     const dispatch = useDispatch<HumanResources>();
     const navigate = useNavigate();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
-
-
+    const [error, setError] = useState(false);
 
     const handleLogin = async () => {
+        const result = await dispatch(fetchLogin({ email, password })).unwrap();
 
-            const result = await dispatch(fetchLogin({ email, password })).unwrap();
+        // `result` içinde `code` özelliği olup olmadığını kontrol edin
+        if (result.code) {
+            setError(result.message);
+            return; // İşlemi sonlandırarak sonraki then bloklarına geçişi engeller.
+        }
 
-            // `result` içinde `code` özelliği olup olmadığını kontrol edin
-            if (result.code) {
-                alert('Username or Password wrong');
-                return; // İşlemi sonlandırarak sonraki then bloklarına geçişi engeller.
-            }
-
-            // `token`'ı kullanarak kullanıcıyı bul
-            dispatch(fetchFindUserByToken(result.token))
-
-                .then(data => {
-                    if (data.payload.userType === 'ADMIN') {
-                        navigate('/admin-page');
-                    } else {
-                        console.log('User is not an ADMIN.');
-                    }
-                });
-
+        // `token`'ı kullanarak kullanıcıyı bul
+        dispatch(fetchFindUserByToken(result.token))
+            .then(data => {
+                if (data.payload.userType === 'ADMIN') {
+                    navigate('/admin-page');
+                } else {
+                    console.log('User is not an ADMIN.');
+                }
+            });
     };
-
-
-
-
 
     return (
         <Paper elevation={6} square sx={{width: '100%', maxWidth: 400}}>
@@ -77,61 +64,74 @@ export default function LoginCard() {
                     Login
                 </Typography>
 
-
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-
-                            onChange={event => setEmail(event.target.value)}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-
-                            onChange={event => setPassword(event.target.value)}
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary"/>}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="button"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                            onClick={() => handleLogin()}
+                {error && (
+                    <Box sx={{ width: '100%' }}>
+                        <Alert severity="error"
+                               sx={{
+                                   width: '100%',
+                                   textAlign: 'center', // Metni ortala
+                                   display: 'flex',
+                                   flexDirection: 'column',
+                                   alignItems: 'center',
+                               }}
                         >
-                            Log In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                {/* <Link href="#" variant="body2">
+
+                            {error+ ' '}
+                            <strong>Try Again!</strong>
+                        </Alert>
+                    </Box>
+                )}
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    onChange={event => setEmail(event.target.value)}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={event => setPassword(event.target.value)}
+                />
+                <FormControlLabel
+                    control={<Checkbox value="remember" color="primary"/>}
+                    label="Remember me"
+                />
+                <Button
+                    type="button"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt: 3, mb: 2}}
+                    onClick={handleLogin}
+                >
+                    Log In
+                </Button>
+                <Grid container>
+                    <Grid item xs>
+                        {/* <Link href="#" variant="body2">
                             Forgot password?
                         </Link> */}
-                            </Grid>
-                            <Grid item>
-                                <Typography variant='body2'>
-                                    <Link to={'/register'}>
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Box>
-
-
+                    </Grid>
+                    <Grid item>
+                        <Typography variant='body2'>
+                            <Link to={'/register'}>
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
         </Paper>
     );
 }

096317a21c5c5965d5a80767cbc7a0bd5edf83a9
candenizgumus <denizgumus1996@hotmail.com>
2024-08-05 14:58:14 +0300

    holidaytable fix

--- human-resource-react/src/components/organisms/HolidayTable.tsx
+++ human-resource-react/src/components/organisms/HolidayTable.tsx
@@ -1,27 +1,42 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {DataGrid, GridColDef, GridRowSelectionModel} from '@mui/x-data-grid';
-import {HumanResources, RootState, useAppSelector} from '../../store';
-import {fetchDeleteHoliday, fetchHolidays} from '../../store/feature/holidaySlice';
-import {IHoliday} from '../../models/IHoliday';
-import {Button, Grid, Box, Divider} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { DataGrid, GridColDef, GridRowSelectionModel } from '@mui/x-data-grid';
+import { HumanResources, RootState, useAppSelector } from '../../store';
+import { fetchDeleteHoliday, fetchHolidays } from '../../store/feature/holidaySlice';
+import { IHoliday } from '../../models/IHoliday';
+import { Button, Grid, Box, Divider } from '@mui/material';
 import HolidayFormSection from "../molecules/HolidayFormSection";
+import { IHolidayFormatted } from "../../models/IHolidayFormatted";
+
+// Helper function to format epoch timestamp to human-readable date
+function epochToHumanReadableWithoutTime(epochTime: number): string {
+    const date = new Date(epochTime * 1000);
+
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        //timeZoneName: 'short'
+    };
+
+    return date.toLocaleDateString('tr-TR', options);
+}
 
 // Define the columns
 const columns: GridColDef[] = [
-    {field: 'id', headerName: 'ID', width: 70},
-    {field: 'holidayName', headerName: 'Holiday', width: 130},
-    {field: 'holidayType', headerName: 'Type', width: 160},
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'holidayName', headerName: 'Holiday', width: 250 },
+    { field: 'holidayType', headerName: 'Type', width: 160 },
     {
         field: 'holidayStartDate',
         headerName: 'Start Date',
-        width: 130,
+        width: 200,
     },
     {
         field: 'holidayEndDate',
         headerName: 'End Date',
-        width: 130,
+        width: 200,
     },
 ];
 
@@ -29,10 +44,22 @@ export default function HolidayTable() {
     const holidays: IHoliday[] = useAppSelector((state: RootState) => state.holiday.holidayList);
     const dispatch = useDispatch<HumanResources>();
     const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
+    const [newHolidayList, setNewHolidayList] = useState<IHolidayFormatted[]>([]);
 
     useEffect(() => {
         dispatch(fetchHolidays());
     }, [dispatch]);
+
+    useEffect(() => {
+        const formattedHolidays = holidays.map(holiday => ({
+            id: holiday.id,
+            holidayName: holiday.holidayName,
+            holidayType: holiday.holidayType,
+            holidayStartDate: epochToHumanReadableWithoutTime(holiday.holidayStartDate),
+            holidayEndDate: epochToHumanReadableWithoutTime(holiday.holidayEndDate),
+        }));
+        setNewHolidayList(formattedHolidays);
+    }, [holidays]);
 
     const handleRowSelection = (newSelectionModel: GridRowSelectionModel) => {
         setSelectedRowIds(newSelectionModel as number[]);
@@ -47,16 +74,16 @@ export default function HolidayTable() {
     };
 
     return (
-        <Box sx={{flexGrow: 1, padding: 2}}>
+        <Box sx={{ flexGrow: 1, padding: 2 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <div style={{height: 400, width: '100%'}}>
+                    <div style={{ height: 400, width: '100%' }}>
                         <DataGrid
-                            rows={holidays}
+                            rows={newHolidayList}
                             columns={columns}
                             initialState={{
                                 pagination: {
-                                    paginationModel: {page: 1, pageSize: 5},
+                                    paginationModel: { page: 1, pageSize: 5 },
                                 },
                             }}
                             pageSizeOptions={[5, 10]}
@@ -75,13 +102,14 @@ export default function HolidayTable() {
                                 },
                             }}
                         />
-                        <Grid container spacing={2} style={{marginTop: 16}}>
+                        <Grid container spacing={2} style={{ marginTop: 16 }}>
                             <Grid item xs={12}>
-                                <Button style={{marginRight: 4}}
-                                        onClick={handleConfirmSelection}
-                                        variant="contained"
-                                        color="primary"
-                                        disabled={true}
+                                <Button
+                                    style={{ marginRight: 4 }}
+                                    onClick={handleConfirmSelection}
+                                    variant="contained"
+                                    color="primary"
+                                    disabled={true}
                                 >
                                     Confirm Selection
                                 </Button>
@@ -97,10 +125,10 @@ export default function HolidayTable() {
                     </div>
                 </Grid>
                 <Grid item xs={12}>
-                    <Divider sx={{my: 4}}/>
+                    <Divider sx={{ my: 4 }} />
                 </Grid>
                 <Grid item xs={12}>
-                    <HolidayFormSection/>
+                    <HolidayFormSection />
                 </Grid>
             </Grid>
         </Box>

82c8ebd7d82d847d72116f086abfc608f6d4a040
emiresen <emiresen246@gmail.com>
2024-08-21 11:28:04 +0300

    login card style

--- human-resource-react/src/pages/RouterPage.tsx
+++ human-resource-react/src/pages/RouterPage.tsx
@@ -1,13 +1,12 @@
-import React, {lazy, Suspense, useEffect} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { lazy, Suspense, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './login_register_pages/Login';
-import Register from './login_register_pages/Register';
 import GetOffer from '../pages/pre_autorize_pages/GetOffer'
 import LandingPage from "./pre_autorize_pages/Landing";
 import UserStoriesPage from "./pre_autorize_pages/UserStoriesPage";
-import {useDispatch} from "react-redux";
-import {HumanResources, useAppSelector} from "../store";
-import {setToken} from "../store/feature/authSlice";
+import { useDispatch } from "react-redux";
+import { HumanResources, useAppSelector } from "../store";
+import { setToken } from "../store/feature/authSlice";
 import AboutUsPage from './pre_autorize_pages/AboustUs';
 import ContactPage from './pre_autorize_pages/ContactPage';
 import Loader from '../components/atoms/loader/Loader';
@@ -36,23 +35,22 @@ const RouterPage = () => {
 
     return (
         <Router>
-            <Suspense fallback={<Loader/>}>
+            <Suspense fallback={<Loader />}>
                 <Routes>
-                    <Route path="/" element={<LandingPage/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/get-offer" element={<GetOffer/>}/>
-                    <Route path="/user-stories" element={<UserStoriesPage/>}/>
-                    <Route path="/features" element={<Features/>}/>
-                    <Route path="/features/Recruitment" element={<FeatureRecruitment/>}/>
-                    <Route path="/features/Performance" element={<FeaturePerformance/>}/>
-                    <Route path="/features/Shift" element={<FeatureShift/>}/>
-                    <Route path="/about-us" element={<AboutUsPage/>}/>
-                    <Route path="/contact" element={<ContactPage/>}/>
-                    <Route path='/admin-home' element={isAuth ? <AdminPage/> : <LandingPage/>}/>
-                    <Route path='/manager-home' element={isAuth ? <ManagerPage/> : <LandingPage/>}/>
-                    <Route path='/employee-home' element={isAuth ? <EmployeePage/> : <LandingPage/>}/>
-                    <Route path={'/password-reset'} element={<PasswordResetPage/>}/>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/get-offer" element={<GetOffer />} />
+                    <Route path="/user-stories" element={<UserStoriesPage />} />
+                    <Route path="/features" element={<Features />} />
+                    <Route path="/features/Recruitment" element={<FeatureRecruitment />} />
+                    <Route path="/features/Performance" element={<FeaturePerformance />} />
+                    <Route path="/features/Shift" element={<FeatureShift />} />
+                    <Route path="/about-us" element={<AboutUsPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path='/admin-home' element={isAuth ? <AdminPage /> : <LandingPage />} />
+                    <Route path='/manager-home' element={isAuth ? <ManagerPage /> : <LandingPage />} />
+                    <Route path='/employee-home' element={isAuth ? <EmployeePage /> : <LandingPage />} />
+                    <Route path={'/password-reset'} element={<PasswordResetPage />} />
                     <Route path="/user-stories/:companyName" element={<UserStoryDetailPage />} />
                 </Routes>
             </Suspense>

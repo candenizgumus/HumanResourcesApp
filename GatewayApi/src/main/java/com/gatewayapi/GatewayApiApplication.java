7b828884a399b006dbaec9669754d6887dc0c485
candenizgumus <denizgumus1996@hotmail.com>
2024-08-22 19:56:30 +0300

    Gateway

---
+++ GatewayApi/src/main/java/com/gatewayapi/GatewayApiApplication.java
@@ -0,0 +1,15 @@
+package com.gatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GatewayApiApplication.class, args);
+    }
+
+}

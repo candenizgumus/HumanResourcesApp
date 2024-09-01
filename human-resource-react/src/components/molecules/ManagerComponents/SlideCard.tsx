a96618fda7f6e1be798c8186dac8fa7bd5656dae
candenizgumus <denizgumus1996@hotmail.com>
2024-09-01 18:17:50 +0300

    SlideCard.tsx

--- human-resource-react/src/components/molecules/ManagerComponents/SlideCard.tsx
+++ human-resource-react/src/components/molecules/ManagerComponents/SlideCard.tsx
@@ -22,12 +22,15 @@ import { HumanResources, useAppSelector } from "../../../store";
 import { changePageState, fetchFindCompanyNameAndManagerNameOfUser } from "../../../store/feature/authSlice";
 import { useDispatch } from "react-redux";
 import Swal from "sweetalert2";
-import { myErrorColour, myLightColour } from "../../../util/MyColours";
+import {myBackgroundColour, myErrorColour, myLightColour} from "../../../util/MyColours";
 import { fetchDeleteTask, fetchGetTasks } from "../../../store/feature/TaskSlice";
 import { ActivateIcon } from '../../atoms/icons';
 import EncoderDecoder from "../../../util/EncoderDecoder";
+
+
+
 const CustomCard = styled(Card)(({ theme }) => ({
-    height: '100%',
+    maxHeight: '100%',
     display: 'flex',
     flexDirection: 'column',
     margin: 'auto',
@@ -124,31 +127,57 @@ const SlideCard = (props: { slide: ISlide, open: boolean, fetchMethod: () => voi
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', padding: '10px', borderRadius: '5px', height: '100%', boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19)' }}>
             <Typography variant="subtitle1" sx={{ textAlign: 'center',display: displayExtras ? 'block' : 'none'}} >
-                <Button onClick={handleGetId}>
-                    {props.slide.id}
-                </Button>
             </Typography>
-            <CustomCard onClick={handleClick}>
-                <img src={RestApis.staticUploads + props.slide.desktopImageUrls[0]} alt="Slide" style={{ width: '100%', objectFit: 'cover' }} />
-            </CustomCard>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-                <Button variant="contained" color="success" sx={{
-                    flex: 1,
-                    margin: '5px',
-                    maxWidth: { xs: '100%', sm: '150px' },
-                    display:  displayExtras ? 'block' : 'none',
-                }} startIcon={<ContentCopyIcon />} onClick={() => setOpenGetLinkModal(true)}>
-                    Get Link
-                </Button>
-                <Button variant="contained" color="error" sx={{
-                    flex: 1,
-                    margin: '5px',
-                    display: displayExtras ? 'block' : 'none',
-                    maxWidth: { xs: '100%', sm: '150px' },
-                }} startIcon={<DeleteIcon />} onClick={handleDeleteSlide}>
-                    Delete
-                </Button>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <CustomCard onClick={handleClick}>
+                        <img src={RestApis.staticUploads + props.slide.desktopImageUrls[0]} alt="Slide" style={{ width: '100%', objectFit: 'cover'  }} />
+                    </CustomCard>
+                </Grid>
+            </Grid>
+            <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4} sx={{ overflow: 'auto' }}>
+                                <Typography sx={{ mb: 1.5, fontStyle: 'italic', fontSize: '12px' }} variant="body2">
+                                    <strong>Id:</strong> {props.slide.id}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5, fontStyle: 'italic', fontSize: '12px' }} variant="body2">
+                                    <strong>City:</strong> {props.slide.city}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5, fontStyle: 'italic', fontSize: '12px' }} variant="body2">
+                                    <strong>District:</strong> {props.slide.district}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={8} sx={{ overflow: 'auto' }}>
+                                <Typography sx={{ mb: 1.5, fontStyle: 'italic', fontSize: '12px' }} variant="body2">
+                                    <strong>N.hood:</strong> {props.slide.neighborhood}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5, fontStyle: 'italic', fontSize: '12px' }} variant="body2">
+                                    <strong>Projection:</strong> {props.slide.projection}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5, fontStyle: 'italic', fontSize: '12px' }} variant="body2">
+                                    <strong>Concept:</strong> {props.slide.concept}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} sx={{ marginTop: '10px', justifyContent: 'flex-end' }}>
+                            <Grid item xs={6}>
+                                <Button fullWidth variant="contained" color="primary" sx={{ margin: '5px' }} startIcon={<ContentCopyIcon />} onClick={() => setOpenGetLinkModal(true)}>
+                                    Get Link
+                                </Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button fullWidth variant="contained" color="error" sx={{ margin: '5px' }} startIcon={<DeleteIcon />} onClick={handleDeleteSlide}>
+                                    Delete
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Box>
+
 
             {/* Dialog and Snackbar components here */}
             <Dialog open={openGetLinkModal} onClose={() => setOpenGetLinkModal(false)} fullWidth maxWidth='sm'>
